42a4b1cceb6db0ea0e7badd6e40e7c6bf632a3d4
fangwenyu <haying2005@163.com>
2018-02-08 10:29:19 +0800

    增加注释

--- webpack.common.js
+++ webpack.common.js
@@ -6,7 +6,7 @@ const webpack = require('webpack');
 module.exports = {
   entry: {
     index: './src/index.js',
-    vendor: ['lodash'],
+    vendor: ['lodash'], //第三方库打到单独的文件里
   },
   output: {
     filename: '[name].[chunkhash].js',
@@ -18,14 +18,15 @@ module.exports = {
     new webpack.BannerPlugin('版权所有，翻版必究'),
 
     new HtmlWebpackPlugin({ //自动生成html文件
-      //template: __dirname + "/app/index.tmpl.html"
+      //template: __dirname + "/app/index.tmpl.html",
+      title: 'webpack demo',
     }),
 
     //使用hash格式的模块id 避免因为模块id改变而产生chunkhash的改变，导致打包后的filename产生不必要的缓存失效
     new webpack.HashedModuleIdsPlugin(),
 
     new webpack.optimize.CommonsChunkPlugin({
-      names: ['common', 'vendor'],
+      names: ['common', 'vendor'],  //注意顺序
       minChunks: 2,
     }),
 
